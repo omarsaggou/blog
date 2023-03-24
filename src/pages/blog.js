b0df2e9196fd9960de0f72b3eedd55e7082f420c
SAGGOU <omarsaggou@gmail.com>
2023-03-24 15:05:54 +0100

    commit

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,10 +2,16 @@ import * as React from 'react'
 import Layout from '../layout/layout'
 import Seo from '../components/seo'
 
-const BlogPage = () => {
+import { graphql } from 'gatsby'
+
+const BlogPage = (data) => {
     return (
         <Layout pageTitle="My Blog Posts">
-            <p>My cool posts will go in here</p>
+            <ul>
+                {data.data.allFile.nodes.map(node => (
+                    <li key={node.name}>{node.name}</li>
+                ))}
+            </ul>
         </Layout>
     )
 }
@@ -13,3 +19,13 @@ const BlogPage = () => {
 export const Head = () => <Seo title="My Blog Posts" />
 
 export default BlogPage
+
+
+export const query = graphql`
+    query {
+        allFile {
+        nodes {
+            name
+        }
+        }
+    }`
