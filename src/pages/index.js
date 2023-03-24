af874bb8e2ecacb104f7d881f32474236402bb0a
SAGGOU <omarsaggou@gmail.com>
2023-03-24 10:39:00 +0100

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import * as React from "react"
 import Layout from './layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>We just started learning Gatsby.</p>
+
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/pic01.png"
+      />
+
     </Layout>
   )
 }
